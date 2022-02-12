a8ffedad6f5ec6ba15c606e491e6cd883a0e8b8b
Roman Cheplyaka <roma@ro-che.info>
2022-02-12 21:26:11 +0200

    Update to a new streaming version

--- src/System/Posix/Directory/Stream.hs
+++ src/System/Posix/Directory/Stream.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE OverloadedStrings, FlexibleContexts, ScopedTypeVariables #-}
+{-# OPTIONS_GHC -Wno-orphans #-}
 module System.Posix.Directory.Stream (streamDirectory, find) where
 
 -- Loosely based on
@@ -6,17 +7,18 @@ module System.Posix.Directory.Stream (streamDirectory, find) where
 
 import Streaming (Stream, Of)
 import qualified Streaming as S
+import Streaming.Internal (Stream(..))
 import qualified Streaming.Prelude as S
 import System.IO.Error
 import System.Posix.Files.ByteString
 import System.Posix.ByteString.FilePath
 import System.Posix.Directory.ByteString
 import qualified Data.ByteString.Char8 as BS8
-import Data.Monoid
 import Control.Exception (IOException)
 import Control.Monad
 import Control.Monad.Catch
 import Control.Monad.IO.Class
+import Control.Monad.Trans.Resource
 
 -- | Return the directory contents as a 'Stream'. The stream does not
 -- include @.@ and @..@ entries.
@@ -27,11 +29,11 @@ import Control.Monad.IO.Class
 -- For an example on how to use this function, see the source code of the 'find'
 -- function.
 streamDirectory
-  :: (S.MonadResource m, MonadCatch m)
+  :: (MonadResource m, MonadCatch m)
   => RawFilePath
   -> Stream (Of RawFilePath) m ()
 streamDirectory path = handle_errors $
-  S.bracketStream
+  bracketStream
     (openDirStream path)
     closeDirStream
     loop
@@ -56,7 +58,7 @@ streamDirectory path = handle_errors $
 -- so you may want to write your own recursive traversal which does exactly
 -- what you need.
 find
-  :: forall m . (S.MonadResource m, MonadCatch m)
+  :: forall m . (MonadResource m, MonadCatch m)
   => RawFilePath
   -> Stream (Of RawFilePath) m ()
 -- Note on error handling. The two most common errors one needs to deal
@@ -78,3 +80,37 @@ find base = handle (\(_ :: IOException) -> return ()) $ loop base where
 
     when is_dir $ do
       S.for (S.map ((path <> "/") <>) $ streamDirectory path) loop 
+
+-- | This function was unfortunately removed from streaming starting with 0.2,
+-- see <https://github.com/haskell-streaming/streaming/issues/36>.
+bracketStream
+  :: (Functor f, MonadResource m)
+  => IO a -> (a -> IO ()) -> (a -> Stream f m b) -> Stream f m b
+bracketStream alloc free inside = do
+        (key, seed) <- S.lift (allocate alloc free)
+        clean key (inside seed)
+  where
+    clean key = loop where
+      loop str = case str of
+        Return r -> Effect (release key >> return (Return r))
+        Effect m -> Effect (liftM loop m)
+        Step f   -> Step (fmap loop f)
+{-#INLINABLE bracketStream #-}
+
+-- Likewise, the following instances were removed from streaming
+
+instance (MonadThrow m, Functor f) => MonadThrow (Stream f m) where
+  throwM = S.lift . throwM
+  {-#INLINE throwM #-}
+
+instance (MonadCatch m, Functor f) => MonadCatch (Stream f m) where
+  catch str f0 = go str
+    where
+    go p = case p of
+      Step f      -> Step (fmap go f)
+      Return  r   -> Return r
+      Effect  m   -> Effect (catch (do
+          p' <- m
+          return (go p'))
+       (\e -> return (f0 e)) )
+  {-#INLINABLE catch #-}
